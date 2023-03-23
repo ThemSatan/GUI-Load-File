512470a4374f60fdcbe0949f7e05cff7902fe488
ThemSatan <91114584+ThemSatan@users.noreply.github.com>
2023-03-23 20:04:03 +0100

    Add files via upload

--- GUIfileLoad/src/Demo.java
+++ GUIfileLoad/src/Demo.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -13,13 +13,26 @@ public class Demo extends JFrame {
     private JPanel panelMain;
     private JButton loadButton;
     private JTextArea textArea;
-    private JButton unloadButton;
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem menuItem;
     public File selectedFile;
+
 
     String line;
     String parametry = "";
 
-    private void FileReader() {
+    public Demo() {
+        initComponents();
+        loadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fileRead();
+                textArea.append(parametry + "\n");
+            }
+        });
+    }
+    private void fileRead() {
         JFileChooser fc = new JFileChooser();
         fc.setFileFilter(new FileNameExtensionFilter("Text files", "txt"));
         int result = fc.showOpenDialog(this);
@@ -28,7 +41,9 @@ public class Demo extends JFrame {
 
             try (Scanner scanner = new Scanner(
                     new BufferedReader(
-                            new FileReader(selectedFile)))) {
+                            new FileReader(selectedFile)
+                    )
+            )) {
                 while (scanner.hasNextLine()) {
                     line = scanner.nextLine();
                     parametry += line;
@@ -39,21 +54,25 @@ public class Demo extends JFrame {
         }
     }
 
-    public Demo() {
-        loadButton.addActionListener(new ActionListener() {
+    private void initComponents(){
+        menuBar = new JMenuBar();
+
+        menu = new JMenu("Menu");
+        menu.setMnemonic(KeyEvent.VK_F);
+        menuBar.add(menu);
+
+        menuItem = new JMenuItem("File");
+        menu.add(menuItem);
+
+        menuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                FileReader();
+                fileRead();
                 textArea.append(parametry + "\n");
             }
         });
 
-        unloadButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                textArea.setText("");
-            }
-        });
+        setJMenuBar(menuBar);
     }
 
     public static void main(String[] args) {
